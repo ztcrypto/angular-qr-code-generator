590840c4dffdea5c0e9845d376f77901233dbbd9
Priest <>
2019-09-14 15:18:05 +0300

    Download image saving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as QRCode from 'easyqrcodejs';
+import domtoimage from 'dom-to-image';
 
 @Component({
   selector: 'app-root',
@@ -15,39 +16,38 @@ export class AppComponent {
   public position = '';
   public phone = '';
   public email = '';
-  public url = location.href;
+  public url = 'intergreatme-qr.herokuapp.com';
   public notes = '';
 
   generateQRCode(): void {
-    var codeText = "BEGIN:VCARD\r\nVERSION:3.0\r\nN:";
-    codeText += this.lname + ";" + this.fname + "\r\n";
-    codeText += "FN:" + this.fname + " " + this.lname + "\r\n";
-    codeText += "ORG:" + this.position + "\r\n";
-    codeText += "URL:" + this.url + "\r\n";
-    codeText += "EMAIL;TYPE=INTERNET:" + this.email + "\r\n";
-    codeText += "TEL:" + this.phone + "\r\n";
-    codeText += "NOTE:" + "Click the link above to make your own QR Code"+ "\r\n";
-    codeText += "END:VCARD";
+
+    let codeText = 'BEGIN:VCARD\r\nVERSION:3.0\r\nN:';
+    codeText += this.lname + ';' + this.fname + '\r\n';
+    codeText += 'FN:' + this.fname + ' ' + this.lname + '\r\n';
+    codeText += 'ORG:' + this.position + '\r\n';
+    codeText += 'URL:' + this.url + '\r\n';
+    codeText += 'EMAIL;WORK:' + this.email + '\r\n';
+    codeText += 'TEL;WORK:' + this.phone + '\r\n';
+    codeText += 'NOTE:' + 'Click the link above to make your own QR Code' + '\r\n';
+    codeText += 'END:VCARD';
     codeText = decodeURIComponent(codeText);
 
     console.log(codeText);
-    console.log(codeText.length);
-    console.log(screen.width);
-    var imagesize = 150;
-    if(screen.width < 480) imagesize = 100;
+    let imagesize = 150;
+    if (screen.width < 480) { imagesize = 120; }
     console.log(imagesize);
-    this.gname = this.fname.split(" ")[0];
+    this.gname = this.fname.split(' ')[0];
 
     const config = {
-      title: "Logo",
+      title: 'Logo',
       config: {
         text: codeText, // Content
         width: imagesize, // Widht
         height: imagesize, // Height
-        colorDark: "#000000", // Dark color
-        colorLight: "#ffffff", // Light color
+        colorDark: '#000000', // Dark color
+        colorLight: '#ffffff', // Light color
         // === Logo
-        logo: "assets/logo.png", // LOGO
+        logo: 'assets/logo.png', // LOGO
         logoBackgroundColor: '#ffffff', // Logo backgroud color, Invalid when `logBgTransparent` is true; default is '#ffffff'
         logoBackgroundTransparent: false, // Whether use transparent image, default is false
         correctLevel: QRCode.CorrectLevel.M // L, M, Q, H
@@ -55,5 +55,23 @@ export class AppComponent {
     };
     if (this.qrcode != null) { this.qrcode.clear(); }
     this.qrcode = new QRCode(document.getElementById('qrcode'), config.config);
+    document.querySelector('#qrcode img').setAttribute('style', 'width: 120px');
+   // document.querySelector('#qrcode img').clientWidth = 120;
+  }
+  saveImage(): void {
+    domtoimage.toPng(document.querySelector('.image-content'))
+    .then((dataUrl: string) => {
+        // tslint:disable-next-line: prefer-const
+        let img = new Image();
+        img.src = dataUrl;
+        const link = document.createElement('a');
+        link.download = 'qr-screen.png';
+        link.href = dataUrl;
+        link.click();
+        link.remove();
+    })
+    .catch((error: any) => {
+        console.error('oops, something went wrong!', error);
+    });
   }
 }
